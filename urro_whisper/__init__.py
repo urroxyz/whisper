569910fc9bdae9dc5727e85aa3c4520b3b4267db
Urro <me@urro.xyz>
2025-04-23 11:42:07 -0400

    Update __init__.py

--- urro_whisper/__init__.py
+++ urro_whisper/__init__.py
@@ -1,8 +1,19 @@
 from .core import whisperer
+from bidi.mirror import MIRRORED
 
-HYPHEN = " -"
-GREATER = " >"
-DOUBLE_GREATER = " >>"
+def HYPHEN(count: int = 1) -> str:
+    return " " + "-" * count
+
+def GREATER_THAN(count: int = 1) -> str:
+    return " " + ">" * count
+
+def SPEAKER(ch: str = "[") -> str:
+    mirror = MIRRORED.get(ch, ch)
+    return " " + ch + "SPEAKER 1" + mirror
+
+def PERSON(ch: str = "[") -> str:
+    mirror = MIRRORED.get(ch, ch)
+    return " " + ch + "PERSON 1" + mirror
 
 __license__ = "GPLv3"
 __version__ = "0.1.0"
